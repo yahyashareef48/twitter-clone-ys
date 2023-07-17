c0f6cb33ecea2d2e0b8f3506706f539f499cc4bb
YahyaShareef <yahyashareef48@gmail.com>
2023-07-17 18:10:15 +0530

    heading of home page done

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { Route, Routes, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
+
 function App() {
-  return <h1 className="text-9xl">hello world with firebase</h1>;
+  return (
+    <div>
+      <nav>
+        
+      </nav>
+      <div>
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" replace />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </div>
+    </div>
+  );
 }
 
 export default App;
