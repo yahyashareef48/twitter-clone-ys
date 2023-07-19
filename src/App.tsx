210c643ea59d206cb8db6f186566721f422ee2ff
YahyaShareef <yahyashareef48@gmail.com>
2023-07-19 20:00:37 +0530

    profile rout protected

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,12 @@ import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import Auth from "./pages/Auth";
 import Profile from "./pages/Profile";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "./firebase";
 
 function App() {
+  const [user] = useAuthState(auth);
+
   return (
     <div className="flex max-w-[1234px] xl:max-w-[1044px] lg:max-w-[660px] mx-auto">
       <Navbar />
@@ -12,7 +16,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Navigate to="/home" replace />} />
           <Route path="/home" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
+          <Route path="/profile" element={user ? <Profile /> : <Navigate to="/login" replace />} />
           <Route path="/login" element={<Auth />} />
         </Routes>
       </div>
